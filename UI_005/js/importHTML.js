726194850f29c44e8472d4978463ca56e8e39be1
Yeonsu1027 <rito1205@naver.com>
2024-01-05 11:47:49 +0900

    js layout

--- UI_005/js/importHTML.js
+++ UI_005/js/importHTML.js
@@ -4,7 +4,7 @@ const importHTML = (tag, file) => {
   xhr.open("GET", file, true); // file매개변수에 저장된값을 open에 주입
   xhr.onreadystatechange = () => {
     if (xhr.readyState === XMLHttpRequest.DONE) {
-      tag.innerHTML = xhr.responseText();
+      tag.innerHTML = xhr.responseText;
     }
   };
   xhr.send(); // 위: 파일을 열어서, 여기: 나한테 보내줘
